1a86bb85110f782f3cc094671bb217c3a762618e
Woodfish <86752391+thewoodfish@users.noreply.github.com>
2022-06-24 01:02:09 +0100

    Samaritan pallet

--- pallets/template/src/lib.rs
+++ pallets/template/src/lib.rs
@@ -1,102 +1,121 @@
 #![cfg_attr(not(feature = "std"), no_std)]
 
-/// Edit this file to define custom logic or remove it if it is not needed.
-/// Learn more about FRAME and the core library of Substrate FRAME pallets:
-/// <https://docs.substrate.io/v3/runtime/frame>
 pub use pallet::*;
 
-#[cfg(test)]
-mod mock;
-
-#[cfg(test)]
-mod tests;
-
-#[cfg(feature = "runtime-benchmarks")]
-mod benchmarking;
-
-#[frame_support::pallet]
+#[frame_support::pallet] 
 pub mod pallet {
+	use frame_support::dispatch::DispatchResult;
 	use frame_support::pallet_prelude::*;
-	use frame_system::pallet_prelude::*;
+	use frame_system::{pallet_prelude::*};
+
+	use frame_support::traits::{tokens::ExistenceRequirement, Currency, UnixTime};
+	use frame_support::transactional;
+
+	#[cfg(feature = "std")]
+	use frame_support::serde::{Deserialize, Serialize};
+
+	// Handles our pallet's currency abstraction
+    type AccountOf<T> = <T as frame_system::Config>::AccountId;
+    // type BalanceOf<T> =
+    //     <<T as Config>::Currency as Currency<<T as frame_system::Config>::AccountId>>::Balance;
+
+	// #[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	// #[scale_info(skip_type_params(T))]
+	// #[codec(mel_bound())]
+	// pub struct Samaritan<T: Config> {
+	// 	pub sid: AccountOf<T>,
+	// 	pub ipfs_cid: [u8; 46],
+	// 	pub fund: Option<BalanceOf<T>>
+	// }
+
+	#[derive(Clone, Encode, Decode, PartialEq, RuntimeDebug, TypeInfo, MaxEncodedLen)]
+	#[scale_info(skip_type_params(T))]
+	#[codec(mel_bound())]
+	pub struct SignInDetail<T: Config> {
+		pub ip: BoundedVec<u8, T::MaxIPAddress>,
+		pub time: u64
+	}
 
 	/// Configure the pallet by specifying the parameters and types on which it depends.
 	#[pallet::config]
 	pub trait Config: frame_system::Config {
 		/// Because this pallet emits events, it depends on the runtime's definition of an event.
 		type Event: From<Event<Self>> + IsType<<Self as frame_system::Config>::Event>;
+        // type Currency: Currency<Self::AccountId>;
+		type TimeProvider: UnixTime;
+ 
+        #[pallet::constant]
+        type MaxIPAddress: Get<u32>;
 	}
+
 
 	#[pallet::pallet]
 	#[pallet::generate_store(pub(super) trait Store)]
 	pub struct Pallet<T>(_);
 
-	// The pallet's runtime storage items.
-	// https://docs.substrate.io/v3/runtime/storage
-	#[pallet::storage]
-	#[pallet::getter(fn something)]
-	// Learn more about declaring storage items:
-	// https://docs.substrate.io/v3/runtime/storage#declaring-storage-items
-	pub type Something<T> = StorageValue<_, u32>;
-
-	// Pallets use events to inform users when important changes are made.
-	// https://docs.substrate.io/v3/runtime/events-and-errors
 	#[pallet::event]
 	#[pallet::generate_deposit(pub(super) fn deposit_event)]
 	pub enum Event<T: Config> {
-		/// Event documentation should end with an array that provides descriptive names for event
-		/// parameters. [something, who]
-		SomethingStored(u32, T::AccountId),
+		/// a Samaritan just signed in
+        SignIn(T::AccountId, u64, BoundedVec<u8, T::MaxIPAddress>),
+		/// a new Samaritan was just created
+		Created(T::AccountId, u64, BoundedVec<u8, T::MaxIPAddress>)
 	}
+
+	#[pallet::storage]
+    #[pallet::getter(fn sign_ins)]
+    pub(super) type SignIns<T: Config> = StorageMap< _, Twox64Concat, T::AccountId, SignInDetail<T>>;
 
 	// Errors inform users that something went wrong.
 	#[pallet::error]
 	pub enum Error<T> {
-		/// Error names should be descriptive.
-		NoneValue,
-		/// Errors should have helpful documentation associated with them.
-		StorageOverflow,
+		/// Samaritan is not existent
+        SamaritanNotExist,
+		
 	}
 
-	// Dispatchable functions allows users to interact with the pallet and invoke state changes.
-	// These functions materialize as "extrinsics", which are often compared to transactions.
-	// Dispatchable functions must be annotated with a weight and must return a DispatchResult.
 	#[pallet::call]
 	impl<T: Config> Pallet<T> {
-		/// An example dispatchable that takes a singles value as a parameter, writes the value to
-		/// storage and emits an event. This function must be dispatched by a signed extrinsic.
-		#[pallet::weight(10_000 + T::DbWeight::get().writes(1))]
-		pub fn do_something(origin: OriginFor<T>, something: u32) -> DispatchResult {
-			// Check that the extrinsic was signed and get the signer.
-			// This function will return an error if the extrinsic is not signed.
-			// https://docs.substrate.io/v3/runtime/origins
-			let who = ensure_signed(origin)?;
+		/// record latest import/creation of Samaritan
+		#[pallet::weight(1000)]
+		pub fn sign_in(origin: OriginFor<T>, ip_addr: BoundedVec<u8, T::MaxIPAddress>) -> DispatchResult
+		{
+			// first check for if account id is present
+			let sender = ensure_signed(origin)?;
+			let user_id = sender.clone();
 
-			// Update storage.
-			<Something<T>>::put(something);
+			let now: u64 = T::TimeProvider::now().as_secs();
 
-			// Emit an event.
-			Self::deposit_event(Event::SomethingStored(something, who));
-			// Return a successful DispatchResultWithPostInfo
+			let nsd: SignInDetail<T> = SignInDetail {
+				ip: ip_addr.clone(),
+				time: now
+			};
+
+			// select user
+			let detail = <SignIns<T>>::get(&user_id);
+
+			if let Some(_sd) = detail {
+				// update timestamp
+				<SignIns<T>>::mutate(&user_id, |det| {
+					*det = Some(nsd);
+				});
+			} else {
+				// user was just created
+				Self::deposit_event(Event::Created(sender, now, ip_addr.clone()));
+
+				// create new signin record and update timestamp 
+				<SignIns<T>>::insert(&user_id, nsd);
+			}
+
+            Self::deposit_event(Event::SignIn(user_id, now, ip_addr.clone()));
+
 			Ok(())
 		}
+	
+	}
 
-		/// An example dispatchable that may throw a custom error.
-		#[pallet::weight(10_000 + T::DbWeight::get().reads_writes(1,1))]
-		pub fn cause_error(origin: OriginFor<T>) -> DispatchResult {
-			let _who = ensure_signed(origin)?;
+	/// helper functions
+	impl<T: Config> Pallet<T> {
 
-			// Read a value from storage.
-			match <Something<T>>::get() {
-				// Return an error if the value has not been set.
-				None => return Err(Error::<T>::NoneValue.into()),
-				Some(old) => {
-					// Increment the value read from storage; will error in the event of overflow.
-					let new = old.checked_add(1).ok_or(Error::<T>::StorageOverflow)?;
-					// Update the value in storage with the incremented result.
-					<Something<T>>::put(new);
-					Ok(())
-				},
-			}
-		}
 	}
 }
