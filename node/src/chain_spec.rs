d793a39cc4e927420e137f06b95a2d1bee28ada7
Woodfish <jasonholt2002@gmail.com>
2022-07-06 10:16:26 +0000

    latest chain spec, addition of ability default

--- node/src/chain_spec.rs
+++ node/src/chain_spec.rs
@@ -1,12 +1,17 @@
+use std::marker::PhantomData;
+
 use node_template_runtime::{
-	AccountId, AuraConfig, BalancesConfig, GenesisConfig, GrandpaConfig, Signature, SudoConfig,
-	SystemConfig, WASM_BINARY,
+	AccountId, AuraConfig, BalancesConfig, CollectiveConfig, GenesisConfig, GrandpaConfig,
+	Signature, SudoConfig, SystemConfig, WASM_BINARY,
 };
 use sc_service::ChainType;
 use sp_consensus_aura::sr25519::AuthorityId as AuraId;
 use sp_core::{sr25519, Pair, Public};
-use sp_finality_grandpa::AuthorityId as GrandpaId; 
+use sp_finality_grandpa::AuthorityId as GrandpaId;
 use sp_runtime::traits::{IdentifyAccount, Verify};
+
+use sc_service::Properties;
+use serde_json::json;
 
 // The URL for the telemetry server.
 // const STAGING_TELEMETRY_URL: &str = "wss://telemetry.polkadot.io/submit/";
@@ -79,6 +84,11 @@ pub fn development_config() -> Result<ChainSpec, String> {
 pub fn local_testnet_config() -> Result<ChainSpec, String> {
 	let wasm_binary = WASM_BINARY.ok_or_else(|| "Development wasm not available".to_string())?;
 
+	let mut props: Properties = Properties::new();
+
+	let value = json!("USD"); // <--- (1)
+	props.insert("tokenSymbol".to_string(), value); // <--- (2)
+
 	Ok(ChainSpec::from_genesis(
 		// Name
 		"Local Testnet",
@@ -117,7 +127,7 @@ pub fn local_testnet_config() -> Result<ChainSpec, String> {
 		// Protocol ID
 		None,
 		// Properties
-		None,
+		Some("USD"),
 		None,
 		// Extensions
 		None,
@@ -132,6 +142,12 @@ fn testnet_genesis(
 	endowed_accounts: Vec<AccountId>,
 	_enable_println: bool,
 ) -> GenesisConfig {
+	let accounts_to_map: Vec<AccountId> = vec![
+		get_account_id_from_seed::<sr25519::Public>("Alice"),
+		get_account_id_from_seed::<sr25519::Public>("Bob"),
+		get_account_id_from_seed::<sr25519::Public>("Charlie"),
+	];
+
 	GenesisConfig {
 		system: SystemConfig {
 			// Add Wasm runtime to storage.
@@ -152,5 +168,10 @@ fn testnet_genesis(
 			key: Some(root_key),
 		},
 		transaction_payment: Default::default(),
+
+		ability: AbilityConfig {
+			phantom: Default::default(),
+			members: accounts_to_map.iter().cloned().map(|x| x).collect(),
+		},
 	}
 }
